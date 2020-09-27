4eb40f09f652862537d425f2ff87709665be813b
Irina Sadovskaya <oderzhanie@gmail.com>
2020-09-27 22:47:06 +0300

    Добавляет отображение попапа объявления на карте

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@
 
 const PIN_WIDTH_HALF = 20;
 const PIN_HEIGHT = 40;
+const OFFER_TYPES_TITLES = {
+  flat: `Квартира`,
+  bungalo: `Бунгало`,
+  house: `Дом`,
+  palace: `Дворец`
+};
+
 const types = [`palace`, `flat`, `house`, `boungalo`];
 const checkTimes = [`12:00`, `13:00`, `14:00`];
 const features = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
@@ -32,7 +39,7 @@ const getRandomArray = function (arr) {
   const randomArrayLength = getRandomNumber(1, arr.length);
   for (let i = 0; i < randomArrayLength; i++) {
 
-    const randomEl = clonedArr.splice(getRandomNumber(0, clonedArr.length - 1), 1)[0];
+    const randomEl = clonedArr.splice(getRandomIndex(clonedArr), 1)[0];
     result.push(randomEl);
   }
   return result;
@@ -82,6 +89,7 @@ const getSimilarObjects = function (quantity) {
 };
 
 getSimilarObjects(pinItems);
+
 const map = document.querySelector(`.map`);
 map.classList.remove(`map--faded`);
 const mapPins = document.querySelector(`.map__pins`);
@@ -100,3 +108,54 @@ for (let i = 0; i < pinItems; i++) {
 }
 
 mapPins.appendChild(fragment);
+
+const cardTemplate = document.querySelector(`#card`).content;
+
+const cardFragment = document.createDocumentFragment();
+
+for (let x = pinItems - 1; x >= 0; x--) {
+  const clonedCard = cardTemplate.cloneNode(true);
+
+  const cardPopup = clonedCard.querySelector(`.map__card`);
+  const cardTitle = cardPopup.querySelector(`.popup__title`);
+  const cardAddress = cardPopup.querySelector(`.popup__text--address`);
+  const cardPrice = cardPopup.querySelector(`.popup__text--price`);
+  const cardType = cardPopup.querySelector(`.popup__type`);
+  const cardCapacity = cardPopup.querySelector(`.popup__text--capacity`);
+  const cardCheckTime = cardPopup.querySelector(`.popup__text--time`);
+  const cardFeatures = cardPopup.querySelector(`.popup__features`);
+  const cardDescription = cardPopup.querySelector(`.popup__description`);
+  const cardPhotos = cardPopup.querySelector(`.popup__photos`);
+  const cardAvatar = cardPopup.querySelector(`.popup__avatar`);
+  cardTitle.textContent = similarObjects[x].offer.title;
+  cardAddress.textContent = similarObjects[x].offer.address;
+  cardPrice.innerHTML = similarObjects[x].offer.price + `&#x20bd;<span>/ночь</span>`;
+  cardType.textContent = OFFER_TYPES_TITLES[similarObjects[x].offer.type];
+
+  cardCapacity.textContent = similarObjects[x].offer.rooms + ` комнаты для ` + similarObjects[x].offer.guests + ` гостей`;
+  cardCheckTime.textContent = `Заезд после ` + similarObjects[x].offer.checkin + `, выезд до ` + similarObjects[x].offer.checkout;
+  for (let f = 0; f < features.length; f++) {
+    const feature = features[f];
+    if (!similarObjects[x].offer.features.includes(feature)) {
+      const featureNode = cardFeatures.querySelector(`.popup__feature--` + feature);
+      featureNode.classList.add(`visually-hidden`);
+    }
+  }
+
+  cardDescription.textContent = similarObjects[x].offer.description;
+  const photosLength = similarObjects[x].offer.photos.length;
+  const firstPhoto = cardPhotos.querySelector(`.popup__photo`);
+  firstPhoto.src = similarObjects[x].offer.photos[0];
+  if (photosLength > 1) {
+    for (let j = 1; j < photosLength; j++) {
+      const clonedPhoto = firstPhoto.cloneNode(true);
+      clonedPhoto.src = similarObjects[x].offer.photos[j];
+      cardPhotos.appendChild(clonedPhoto);
+    }
+  }
+  cardAvatar.src = similarObjects[x].author.avatar;
+  cardFragment.appendChild(clonedCard);
+}
+const mapFilters = document.querySelector(`.map__filters-container`);
+const parentSection = mapFilters.parentNode;
+parentSection.insertBefore(cardFragment, mapFilters);
